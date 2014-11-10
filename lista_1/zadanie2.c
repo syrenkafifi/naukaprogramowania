30fbb06903442f92898693d303444c85e3a26305
Fifi @ Syrenka <magdafedorczuk@gmail.com>
2014-11-10 13:10:22 +0100

    zadanie2

---
+++ lista_1/zadanie2.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#define PI 3.14159
+#define PROMIEN 10
+
+int main(){
+	float objetosc;
+	objetosc = 4/3*PI*PROMIEN*PROMIEN*PROMIEN;
+	printf("Objętość kuli o promieniu 10 wynosi : %.2f \n", objetosc);
+
+	return 0;
+}
