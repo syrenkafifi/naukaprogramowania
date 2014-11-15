ae886e876962cb5c4c5f2ddc6895e916d5909e16
Fifi @ Syrenka <magdafedorczuk@gmail.com>
2014-11-15 16:51:35 +0100

    koniec listy pierwszej

--- lista_1/zadanie11.c
+++ lista_1/zadanie11.c
@@ -1,6 +1,20 @@
 #include "stdio.h"
 
+//skopiowane zadanie 10 musisz pozmieniac
 int main(){
-	
+	//deklaruje zmienna na znaki o rozmiarze 2 (nawiasem mowiac to tablica)
+	//! zmien rozmiar zmiennej
+	char liczba[2];
+
+	//! zmien stringa (nie stringi):P
+	printf("Podaj liczbe 2 cyfrową: ");
+
+	//zapisuje 2 znaki
+	//! a moze jednak 3 znaki?
+	scanf("%2s", liczba);
+
+	//wyswietlam po znaku ale liczac od konca (koniec ale liczymy od zera wiec dostep do elementow mam po 0 i 1, a wskapk 1, 0)
+	//! zmien to i wyswietl 3 znaki wspak
+	printf("wspak: %c%c\n",liczba[1], liczba[0]);	
 	return 0;
 }
